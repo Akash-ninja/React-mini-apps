a1322f8bd1622f001afb30d0c7316ea12f68d26b
Akash-ninja <akash.gitcode@gmail.com>
2023-10-15 01:06:04 +0530

    Error handling code added

--- 12-Use-popcorn/src/App.js
+++ 12-Use-popcorn/src/App.js
@@ -56,17 +56,33 @@ export default function App() {
   const [movies, setMovies] = useState(tempMovieData)
   const [watched, setWatched] = useState(tempWatchedData)
   const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState('')
   const query = 'interstellar'
 
   useEffect(function () {
     async function fetchMovies() {
-      setIsLoading(true)
-      const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
-      )
-      const data = await res.json()
-      setMovies(data.Search)
-      setIsLoading(false)
+      try {
+        setError('')
+        setIsLoading(true)
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+        )
+
+        if (!res.ok) {
+          throw new Error('Something went wrong with fetching movies!')
+        }
+
+        const data = await res.json()
+        if (data.Response === 'False') {
+          throw new Error('Movie not found!')
+        }
+
+        setMovies(data.Search)
+      } catch (err) {
+        setError(err.message)
+      } finally {
+        setIsLoading(false)
+      }
     }
 
     fetchMovies()
@@ -80,7 +96,11 @@ export default function App() {
       </Navbar>
 
       <Main>
-        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
+        <Box>
+          {error && <ErrorMessage message={error} />}
+          {!isLoading && !error && <MovieList movies={movies} />}
+          {isLoading && <Loader />}
+        </Box>
 
         <Box>
           <WatchedSummary watched={watched} />
@@ -93,6 +113,15 @@ export default function App() {
 
 function Loader() {
   return <p className='loader'>Loading...</p>
+}
+
+function ErrorMessage({ message }) {
+  return (
+    <p className='error'>
+      <span>⛔️ </span>
+      {message}
+    </p>
+  )
 }
 
 // structural component
