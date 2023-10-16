0c09f8efaead6d4ddc6c2bbbf6f164f7f9f23123
Akash-ninja <akash.gitcode@gmail.com>
2023-10-16 08:56:16 +0530

    COMMIT AFTER MERGE: Selected Movie details fetched and displayed

--- 12-Use-popcorn/src/App.js
+++ 12-Use-popcorn/src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import StarRating from './StarRating'
 
 const tempMovieData = [
   {
@@ -54,8 +55,8 @@ const KEY = '8bde1356'
 
 export default function App() {
   const [query, setQuery] = useState('inception')
-  const [movies, setMovies] = useState(tempMovieData)
-  const [watched, setWatched] = useState(tempWatchedData)
+  const [movies, setMovies] = useState([])
+  const [watched, setWatched] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState('')
   const [selectedId, setSelectedId] = useState(null)
@@ -235,12 +236,77 @@ function Movie({ movie, onSelectMovie, onCloseMovie }) {
 }
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+  const [movie, setMovie] = useState({})
+  const [isLoading, setIsLoading] = useState(false)
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie
+
+  console.log(title, year)
+
+  useEffect(
+    function () {
+      async function getMovieDetails() {
+        setIsLoading(true)
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+        )
+        const data = await res.json()
+        setMovie(data)
+        setIsLoading(false)
+      }
+      getMovieDetails()
+    },
+    [selectedId]
+  )
+
   return (
     <div className='details'>
-      <button className='btn-back' onClick={onCloseMovie}>
-        &larr;
-      </button>
-      {selectedId}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <header>
+            <button className='btn-back' onClick={onCloseMovie}>
+              &larr;
+            </button>
+            <img src={poster} alt={`Poster of ${movie} movie`} />
+
+            <div className='details-overview'>
+              <h2>{title}</h2>
+              <p>
+                {released} &bull; {runtime}
+              </p>
+              <p>{genre}</p>
+              <p>
+                <span>⭐</span>
+                {imdbRating} IMDb rating
+              </p>
+            </div>
+          </header>
+
+          <section>
+            <div className='rating'>
+              <StarRating maxRating={10} size={24} />
+            </div>
+            <p>
+              <em>{plot}</em>
+            </p>
+            <p>Starring {actors}</p>
+            <p>Directed by {director}</p>
+          </section>
+        </>
+      )}
     </div>
   )
 }
