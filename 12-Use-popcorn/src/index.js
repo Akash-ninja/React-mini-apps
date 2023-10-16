593ca575b58b102fc6a92b6e1af3d825b5d93c3f
Akash-ninja <akash.gitcode@gmail.com>
2023-10-16 07:40:37 +0530

    App-12:: Feature: Star Rating Functionality Implemetation (#2)
    * Added component for Generic Star rating
    
    * default value passed for a setter function

--- 12-Use-popcorn/src/index.js
+++ 12-Use-popcorn/src/index.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
+import StarRating from './StarRating'
 import './index.css'
-import App from './App'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating
+      maxRating={'akjsandjk'}
+      messages={['Terrible', 'Bad', 'Just fine', 'Good', 'Amazing']}
+    />
+    <StarRating size={24} color='red' defaultRating={3} />
+    <Test />
   </React.StrictMode>
 )
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0)
+
+  return (
+    <div>
+      <StarRating color='blue' onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  )
+}
