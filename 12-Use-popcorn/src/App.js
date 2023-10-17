b1a0a85d04793c33e6197c565c15ebb0c74c0024
Akash-ninja <akash.gitcode@gmail.com>
2023-10-17 08:50:29 +0530

    user rating displayed on watched movie list

--- 12-Use-popcorn/src/App.js
+++ 12-Use-popcorn/src/App.js
@@ -155,6 +155,7 @@ export default function App() {
 function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
   const [movie, setMovie] = useState({})
   const [isLoading, setIsLoading] = useState(false)
+  const [userRating, setUserRating] = useState(0)
 
   const {
     Title: title,
@@ -193,6 +194,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
       poster,
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(' ').at(0)),
+      userRating,
     }
 
     onAddWatched(newWatchedMovie)
@@ -226,10 +228,16 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
 
           <section>
             <div className='rating'>
-              <StarRating maxRating={10} size={24} />
-              <button className='btn-add' onClick={handleAdd}>
-                + Add to list
-              </button>
+              <StarRating
+                maxRating={10}
+                size={24}
+                onSetRating={setUserRating}
+              />
+              {userRating > 0 && (
+                <button className='btn-add' onClick={handleAdd}>
+                  + Add to list
+                </button>
+              )}
             </div>
             <p>
               <em>{plot}</em>
