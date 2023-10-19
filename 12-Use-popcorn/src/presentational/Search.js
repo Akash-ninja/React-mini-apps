719077bf2fa9d7288ed6da1d49a34f580d66342a
Akash-ninja <akash.gitcode@gmail.com>
2023-10-19 13:29:52 +0530

    why and how not to select DOM elements

--- 12-Use-popcorn/src/presentational/Search.js
+++ 12-Use-popcorn/src/presentational/Search.js
@@ -1,4 +1,16 @@
 export default function Search({ query, setQuery }) {
+  /*
+      1. Not a convention with React - like its not in Declarative way of React
+      2. Not in line with rest of code
+      3. Not want to add event listeners manaually like this
+      4. Having to add classes or ids just for the purpose of selecting - Not ideal
+      4. If the code re-runs then we are selecting the element over and over again - Not Ideal
+   */
+  /* useEffect(function () {
+    const el = document.querySelector('.search')
+    el.focus()
+  }, []) */
+
   return (
     <input
       className='search'
