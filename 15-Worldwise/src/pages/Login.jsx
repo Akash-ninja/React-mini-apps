47d446ece216d24a96be172858c2044e6c40f870
Akash-ninja <akash.gitcode@gmail.com>
2023-11-08 20:57:58 +0530

    Implemented the Static layout of the app

--- 15-Worldwise/src/pages/Login.jsx
+++ 15-Worldwise/src/pages/Login.jsx
@@ -1,29 +1,32 @@
-import { useState } from "react";
-import styles from "./Login.module.css";
+import { useState } from 'react'
+import styles from './Login.module.css'
+import PageNav from '../components/PageNav'
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
-  const [email, setEmail] = useState("jack@example.com");
-  const [password, setPassword] = useState("qwerty");
+  const [email, setEmail] = useState('jack@example.com')
+  const [password, setPassword] = useState('qwerty')
 
   return (
     <main className={styles.login}>
+      <PageNav />
+
       <form className={styles.form}>
         <div className={styles.row}>
-          <label htmlFor="email">Email address</label>
+          <label htmlFor='email'>Email address</label>
           <input
-            type="email"
-            id="email"
+            type='email'
+            id='email'
             onChange={(e) => setEmail(e.target.value)}
             value={email}
           />
         </div>
 
         <div className={styles.row}>
-          <label htmlFor="password">Password</label>
+          <label htmlFor='password'>Password</label>
           <input
-            type="password"
-            id="password"
+            type='password'
+            id='password'
             onChange={(e) => setPassword(e.target.value)}
             value={password}
           />
@@ -34,5 +37,5 @@ export default function Login() {
         </div>
       </form>
     </main>
-  );
+  )
 }
