4ef3efcd571888293513b0d5f9d906cbacd14d09
Akash-ninja <akash.gitcode@gmail.com>
2023-11-15 15:45:08 +0530

    dynamic routes implemented for different cities

--- 15-Worldwise/src/components/City.jsx
+++ 15-Worldwise/src/components/City.jsx
@@ -1,25 +1,31 @@
-import styles from "./City.module.css";
+import styles from './City.module.css'
+
+import { useParams } from 'react-router-dom'
 
 const formatDate = (date) =>
-  new Intl.DateTimeFormat("en", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-    weekday: "long",
-  }).format(new Date(date));
+  new Intl.DateTimeFormat('en', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    weekday: 'long',
+  }).format(new Date(date))
 
 function City() {
+  const { id } = useParams()
+
   // TEMP DATA
   const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
+    cityName: 'Lisbon',
+    emoji: '🇵🇹',
+    date: '2027-10-31T15:59:59.138Z',
+    notes: 'My favorite city so far!',
+  }
 
-  const { cityName, emoji, date, notes } = currentCity;
+  const { cityName, emoji, date, notes } = currentCity
 
-  return (
+  return <h1>City {id}</h1>
+
+  /* return (
     <div className={styles.city}>
       <div className={styles.row}>
         <h6>City name</h6>
@@ -55,7 +61,7 @@ function City() {
         <ButtonBack />
       </div>
     </div>
-  );
+  ); */
 }
 
-export default City;
+export default City
