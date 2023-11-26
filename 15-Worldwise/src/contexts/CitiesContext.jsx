789ddd32a0684138a49230e18353be86c759c94c
Akash-ninja <akash.gitcode@gmail.com>
2023-11-26 14:06:27 +0530

    Deleting a city

--- 15-Worldwise/src/contexts/CitiesContext.jsx
+++ 15-Worldwise/src/contexts/CitiesContext.jsx
@@ -53,7 +53,22 @@ function CitiesProvider({ children }) {
 
       setCities((cities) => [...cities, data])
     } catch {
-      alert('There was an error loading data...')
+      alert('There was an error creating the city...')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true)
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: 'DELETE',
+      })
+
+      setCities((cities) => cities.filter((city) => city.id !== id))
+    } catch {
+      alert('There was an error deleting city...')
     } finally {
       setIsLoading(false)
     }
@@ -61,7 +76,14 @@ function CitiesProvider({ children }) {
 
   return (
     <CitiesContext.Provider
-      value={{ cities, isLoading, currentCity, getCity, createCity }}
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+        createCity,
+        deleteCity,
+      }}
     >
       {children}
     </CitiesContext.Provider>
