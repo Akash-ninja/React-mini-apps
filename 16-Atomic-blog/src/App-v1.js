9568fb026508794a7a04e9dd1373b709239c21ee
Akash Keshari <akash.gitcode@gmail.com>
2023-11-28 13:33:03 +0530

    App-16: Atomic Blog: Fixing Prop drilling issues  (#8)
    * Fixed: Prop drilling issue via useContext hook
    
    * Custom Provider Hook made for context logics

--- 16-Atomic-blog/src/App-v1.js
+++ 16-Atomic-blog/src/App-v1.js
@@ -1,5 +1,4 @@
-import { useEffect, useState } from 'react'
-import { PostProvider, usePosts } from './PostContext'
+import { createContext, useContext, useEffect, useState } from 'react'
 import { faker } from '@faker-js/faker'
 
 function createRandomPost() {
@@ -9,8 +8,33 @@ function createRandomPost() {
   }
 }
 
+/* CREATING A CONTEXT */
+const PostContext = createContext()
+
 function App() {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  )
+  const [searchQuery, setSearchQuery] = useState('')
   const [isFakeDark, setIsFakeDark] = useState(false)
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts])
+  }
+
+  function handleClearPosts() {
+    setPosts([])
+  }
 
   // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
@@ -21,28 +45,36 @@ function App() {
   )
 
   return (
-    <section>
-      <button
-        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
-        className='btn-fake-dark-mode'
-      >
-        {isFakeDark ? '☀️' : '🌙'}
-      </button>
+    /* PROVDING VALUE TO CHILD COMPONENTS */
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onAddPost: handleAddPost,
+        onClearPosts: handleClearPosts,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
+      <section>
+        <button
+          onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+          className='btn-fake-dark-mode'
+        >
+          {isFakeDark ? '☀️' : '🌙'}
+        </button>
 
-      {/* PROVDING VALUE TO CHILD COMPONENTS */}
-      <PostProvider>
         <Header />
         <Main />
         <Archive />
         <Footer />
-      </PostProvider>
-    </section>
+      </section>
+    </PostContext.Provider>
   )
 }
 
 function Header() {
-  /* CONSUMING THE CONTEXT VALUE via custom provider hook */
-  const { onClearPosts } = usePosts()
+  /* CONSUMING THE CONTEXT VALUE */
+  const { onClearPosts } = useContext(PostContext)
 
   return (
     <header>
@@ -59,7 +91,7 @@ function Header() {
 }
 
 function SearchPosts() {
-  const { searchQuery, setSearchQuery } = usePosts()
+  const { searchQuery, setSearchQuery } = useContext(PostContext)
 
   return (
     <input
@@ -71,7 +103,7 @@ function SearchPosts() {
 }
 
 function Results() {
-  const { posts } = usePosts()
+  const { posts } = useContext(PostContext)
 
   return <p>🚀 {posts.length} atomic posts found</p>
 }
@@ -94,7 +126,7 @@ function Posts() {
 }
 
 function FormAddPost() {
-  const { onAddPost } = usePosts()
+  const { onAddPost } = useContext(PostContext)
 
   const [title, setTitle] = useState('')
   const [body, setBody] = useState('')
@@ -125,7 +157,7 @@ function FormAddPost() {
 }
 
 function List() {
-  const { posts } = usePosts()
+  const { posts } = useContext(PostContext)
 
   return (
     <ul>
@@ -140,7 +172,7 @@ function List() {
 }
 
 function Archive() {
-  const { onAddPost } = usePosts()
+  const { onAddPost } = useContext(PostContext)
 
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
