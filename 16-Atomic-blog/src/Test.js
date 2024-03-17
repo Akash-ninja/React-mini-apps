709bd3dd689028cdf5db042830c9bd34df740187
Akash-ninja <akash.gitcode@gmail.com>
2024-03-17 16:15:13 +0530

    App-19-1: Initial Code Template

--- 16-Atomic-blog/src/Test.js
+++ 16-Atomic-blog/src/Test.js
@@ -14,13 +14,26 @@ function SlowComponent() {
   );
 }
 
-export default function Test() {
-  const [count, setCount] = useState(0);
+function Counter({ children }) {
+  const [count, setCount] = useState(0)
+
   return (
     <div>
       <h1>Slow counter?!?</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-      <SlowComponent />
+
+      {children}
     </div>
-  );
+  )
+}
+
+export default function Test() {
+  return (
+    <div>
+      <h1>Slow counter?!?</h1>
+      <Counter>
+        <SlowComponent />
+      </Counter>
+    </div>
+  )
 }
