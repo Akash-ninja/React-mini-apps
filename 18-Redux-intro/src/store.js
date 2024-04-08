0be734ef6f7894a9f5e3f5f065793027a191a470
Akash-ninja <akash.gitcode@gmail.com>
2024-04-08 06:24:28 +0530

    Addition of Redux

--- 18-Redux-intro/src/store.js
+++ 18-Redux-intro/src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux'
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -14,7 +16,12 @@ function reducer(state = initialState, action) {
 
     case 'account/requestLoan':
       if (state.loan > 0) return state
-      return { ...state, loan: state.loan }
+      return {
+        ...state,
+        loan: action.payload.amount,
+        balance: state.balance + action.payload.amount,
+        loanPurpose: action.payload.purpose,
+      }
 
     case 'account/payLoan':
       return {
@@ -28,3 +35,15 @@ function reducer(state = initialState, action) {
       return state
   }
 }
+
+const store = createStore(reducer)
+
+store.dispatch({ type: 'account/deposit', payload: 100 })
+store.dispatch({ type: 'account/withdraw', payload: 50 })
+console.log(store.getState())
+
+store.dispatch({
+  type: 'account/requestLoan',
+  payload: { amount: 1000, purpose: 'Buy a car' },
+})
+console.log(store.getState())
