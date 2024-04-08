fc1ce60d4608f4b3109d4dfc1f6ff2cdd7d63cd0
Akash-ninja <akash.gitcode@gmail.com>
2024-04-08 08:09:38 +0530

    One more state added: customer

--- 18-Redux-intro/src/store.js
+++ 18-Redux-intro/src/store.js
@@ -1,12 +1,18 @@
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 
-const initialState = {
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: '',
 }
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+  fullName: '',
+  nationalID: '',
+  createdAt: '',
+}
+
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case 'account/deposit':
       return { ...state, balance: state.balance + action.payload }
@@ -36,7 +42,33 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer)
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case 'customer/createCustomer':
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      }
+
+    case 'customer/updateCustomer':
+      return {
+        ...state,
+        fullName: action.payload,
+      }
+
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+})
+
+const store = createStore(rootReducer)
 
 /* store.dispatch({ type: 'account/deposit', payload: 100 })
 store.dispatch({ type: 'account/withdraw', payload: 50 })
@@ -75,4 +107,23 @@ store.dispatch(requestLoan(100))
 console.log(store.getState())
 
 store.dispatch(payLoan(100))
+console.log(store.getState())
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: 'customer/createCustomer',
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  }
+}
+
+function updateCustomer(fullName) {
+  return {
+    type: 'customer/updateCustomer',
+    payload: fullName,
+  }
+}
+
+store.dispatch(createCustomer('Akash Keshri', '20901939'))
+console.log(store.getState())
+store.dispatch(updateCustomer('Akash Keshari'))
 console.log(store.getState())
