cc44496381855f0d74660b7e3237c2f437d9fc32
Akash-ninja <akash.gitcode@gmail.com>
2024-04-09 19:40:57 +0530

    Given a Professional File Structure for our Redux app

--- 18-Redux-intro/src/index.js
+++ 18-Redux-intro/src/index.js
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import store from './store'
 
-import './store'
+store.dispatch({ type: 'account/deposit', payload: 250 })
+console.log(store.getState())
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
