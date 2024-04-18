51b64ee1b2b458e0ce8b6e15d2b85ca798b7e4c1
Akash-ninja <akash.gitcode@gmail.com>
2024-04-18 09:09:19 +0530

    Implemented box model

--- 20-Fast-react-pizza/src/features/cart/CartOverview.jsx
+++ 20-Fast-react-pizza/src/features/cart/CartOverview.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom'
 
 function CartOverview() {
   return (
-    <div className='bg-stone-800 uppercase text-stone-200'>
-      <p className='font-semibold text-stone-300'>
-        <span className='text-stone-300'>23 pizzas</span>
+    <div className='bg-stone-800 p-4 uppercase text-stone-200'>
+      <p className='space-x-4 font-semibold text-stone-300'>
+        <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
       <Link to='/cart'>Open cart &rarr;</Link>
