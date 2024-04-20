0e0184da7a7191f7f3d1a565c8c95a721cb73e3b
Akash-ninja <akash.gitcode@gmail.com>
2024-04-20 10:59:12 +0530

    Style changed for Create  order page alonwith title of the webpage

--- 20-Fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 20-Fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -43,31 +43,40 @@ function CreateOrder() {
   const cart = fakeCart
 
   return (
-    <div>
-      <h2>Ready to order? Let`s go!</h2>
+    <div className='px-4 py-6'>
+      <h2 className='mb-8 text-xl font-semibold'>Ready to order? Let`s go!</h2>
 
       <Form method='POST'>
-        <div>
-          <label>First Name</label>
-          <input className='input' type='text' name='customer' required />
+        <div className='mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'>
+          <label className='sm:basis-40'>First Name</label>
+          <input className='input grow' type='text' name='customer' required />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input className='input' type='tel' name='phone' required />
-          </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
-        </div>
-
-        <div>
-          <label>Address</label>
-          <div>
-            <input className='input' type='text' name='address' required />
+        <div className='mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'>
+          <label className='sm:basis-40'>Phone number</label>
+          <div className='grow'>
+            <input className='input w-full' type='tel' name='phone' required />
+            {formErrors?.phone && (
+              <p className='mt-2 rounded-full bg-red-100 p-2 text-xs text-red-700'>
+                {formErrors.phone}
+              </p>
+            )}
           </div>
         </div>
 
-        <div>
+        <div className='mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'>
+          <label className='sm:basis-40'>Address</label>
+          <div className='grow'>
+            <input
+              className='input w-full'
+              type='text'
+              name='address'
+              required
+            />
+          </div>
+        </div>
+
+        <div className='mb-12 flex items-center gap-5'>
           <input
             className='h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2'
             type='checkbox'
@@ -76,7 +85,9 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor='priority'>Want to yo give your order priority?</label>
+          <label htmlFor='priority' className='font-medium'>
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
@@ -106,12 +117,11 @@ export async function action({ request }) {
       'Please give us your correct phone number. We might need it to contact you'
   }
 
-  // if (Object.keys(errors).length > 0) return errors
+  if (Object.keys(errors).length > 0) return errors
 
-  // const newOrder = await createOrder(order)
+  const newOrder = await createOrder(order)
 
-  // return redirect(`/order/${newOrder.id}`)
-  return null
+  return redirect(`/order/${newOrder.id}`)
 }
 
 export default CreateOrder
