0ca49ab0be8193bf38348f6e956bec796e9d1d76
Akash Keshari <akash.gitcode@gmail.com>
2024-04-27 18:13:24 +0530

    App-15: [PR-7]: Implementation of Fake Authentication flow in Worldwise (#12)
    * Setting up context for authentication
    
    * Some notes added in login flow text file
    
    * Implemented Login and Logout flow
    
    * Homepage main button redirection changed to open login page
    
    * Handled Back navigation after Login
    
    * Routes Protected by a component
    
    * Fix: Protected route not working on reloading the app
    
    * Formatting changes in index html file
    
    * App icon and title changed

--- 15-Worldwise/src/pages/Login.jsx
+++ 15-Worldwise/src/pages/Login.jsx
@@ -1,17 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styles from './Login.module.css'
 import PageNav from '../components/PageNav'
+import { useAuth } from '../contexts/FakeAuthContext'
+import { useNavigate } from 'react-router-dom'
+import Button from '../components/Button'
 
 export default function Login() {
+  const navigate = useNavigate()
+
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState('jack@example.com')
   const [password, setPassword] = useState('qwerty')
+
+  const { login, isAuthenticated } = useAuth()
+
+  useEffect(
+    function () {
+      if (isAuthenticated) {
+        navigate('/app', { replace: true })
+      }
+    },
+    [isAuthenticated, navigate]
+  )
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    if (email && password) login(email, password)
+  }
 
   return (
     <main className={styles.login}>
       <PageNav />
 
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor='email'>Email address</label>
           <input
@@ -33,7 +55,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type='primary'>Login</Button>
         </div>
       </form>
     </main>
