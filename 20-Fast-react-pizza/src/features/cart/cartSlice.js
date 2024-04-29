8c156ca279eca584f31bebb9fd37030c47a58401
Akash-ninja <akash.gitcode@gmail.com>
2024-04-29 07:26:32 +0530

    Implemented: Deleting Cart Items

--- 20-Fast-react-pizza/src/features/cart/cartSlice.js
+++ 20-Fast-react-pizza/src/features/cart/cartSlice.js
@@ -51,3 +51,6 @@ export const getTotalcartQuantity = (state) =>
 
 export const getTotalcartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0)
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0
