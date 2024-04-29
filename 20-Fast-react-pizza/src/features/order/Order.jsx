b1589ba69301aa41a2d7d10ac50fae697e6536fc
Akash-ninja <akash.gitcode@gmail.com>
2024-04-30 00:10:26 +0530

    Fetched ingredients in Cart page without navigation via useFetcher()

--- 20-Fast-react-pizza/src/features/order/Order.jsx
+++ 20-Fast-react-pizza/src/features/order/Order.jsx
@@ -1,5 +1,6 @@
 // Test ID: IIDSAT
-import { useLoaderData } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useFetcher, useLoaderData } from 'react-router-dom'
 import { getOrder } from '../../services/apiRestaurant'
 import {
   calcMinutesLeft,
@@ -10,6 +11,15 @@ import OrderItem from './OrderItem'
 
 function Order() {
   const order = useLoaderData()
+
+  const fetcher = useFetcher()
+
+  useEffect(
+    function () {
+      if (!fetcher.data && fetcher.state === 'idle') fetcher.load('/menu')
+    },
+    [fetcher],
+  )
 
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
@@ -53,7 +63,15 @@ function Order() {
 
       <ul className='divide-y divide-stone-200 border-b border-t'>
         {cart.map((item) => (
-          <OrderItem key={item.pizzaId} item={item} />
+          <OrderItem
+            key={item.pizzaId}
+            item={item}
+            isLoadingIngredients={fetcher.state === 'loading'}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)
+                ?.ingredients ?? []
+            }
+          />
         ))}
       </ul>
 
