877d540982b62c2500c93681d267c6c69e4d0b3a
Akash-ninja <akash.gitcode@gmail.com>
2024-05-02 11:56:49 +0530

    Integrated Supabase and tested it in a component

---
+++ 19-The-wild-oasis/src/services/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js"
+const supabaseUrl = "https://okikpwrjqydvwlukcctx.supabase.co"
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9raWtwd3JqcXlkdndsdWtjY3R4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA1Njg0MDQsImV4cCI6MjAyNjE0NDQwNH0.jHIBEApAxwwPVrGEybG8oKyNIZSrmYD822vD0Qe_ks4"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
+
+/* Exposed supabase key but due to Row Level security (RLS) write is not allowed */
