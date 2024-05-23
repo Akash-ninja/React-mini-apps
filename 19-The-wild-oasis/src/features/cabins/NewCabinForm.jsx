5c29fa840ec9e07567ec6bc2793d85c89123422b
Akash-ninja <akash.gitcode@gmail.com>
2024-05-23 21:21:43 +0530

    created API and invocation for creating new cabin

--- 19-The-wild-oasis/src/features/cabins/NewCabinForm.jsx
+++ 19-The-wild-oasis/src/features/cabins/NewCabinForm.jsx
@@ -1,11 +1,14 @@
-import { useForm } from "react-hook-form"
 import styled from "styled-components"
+import toast from "react-hot-toast"
+import { useForm } from "react-hook-form"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 
 import Input from "../../ui/Input"
 import Form from "../../ui/Form"
 import Button from "../../ui/Button"
 import FileInput from "../../ui/FileInput"
 import Textarea from "../../ui/Textarea"
+import { createCabin } from "../../services/apiCabins"
 
 const FormRow = styled.div`
   display: grid;
@@ -44,10 +47,24 @@ const Error = styled.span`
 `
 
 function NewCabinForm() {
-  const { register, handleSubmit } = useForm()
+  const queryClient = useQueryClient()
+
+  const { mutate, isLoading: isCreating } = useMutation({
+    mutationFn: createCabin,
+    onSuccess: () => {
+      toast.success("New cabin successfully created.")
+      queryClient.invalidateQueries({ queryKey: ["cabins"] })
+      reset()
+    },
+    onError: (err) => {
+      toast.error(err.message)
+    },
+  })
+
+  const { register, handleSubmit, reset } = useForm()
 
   function onSubmit(data) {
-    console.log(data)
+    mutate(data)
   }
 
   return (
@@ -97,7 +114,7 @@ function NewCabinForm() {
         <Button variation='secondary' type='reset'>
           Cancel
         </Button>
-        <Button>Add cabin</Button>
+        <Button disabled={isCreating}>Add cabin</Button>
       </FormRow>
     </Form>
   )
