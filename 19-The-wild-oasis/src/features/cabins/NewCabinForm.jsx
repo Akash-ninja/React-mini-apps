ecb04a8264f6a5cbe36300e7cf3bbdea2c4e70d2
Akash-ninja <akash.gitcode@gmail.com>
2024-05-25 12:01:06 +0530

    Handled Form errors and made separate FomRow component

--- 19-The-wild-oasis/src/features/cabins/NewCabinForm.jsx
+++ 19-The-wild-oasis/src/features/cabins/NewCabinForm.jsx
@@ -8,42 +8,11 @@ import Form from "../../ui/Form"
 import Button from "../../ui/Button"
 import FileInput from "../../ui/FileInput"
 import Textarea from "../../ui/Textarea"
+import FormRow from "../../ui/FormRow"
 import { createCabin } from "../../services/apiCabins"
-
-const FormRow = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
-
-  padding: 1.2rem 0;
-
-  &:first-child {
-    padding-top: 0;
-  }
-
-  &:last-child {
-    padding-bottom: 0;
-  }
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
-`
 
 const Label = styled.div`
   font-weight: 500;
-`
-
-const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
 `
 
 function NewCabinForm() {
@@ -61,46 +30,77 @@ function NewCabinForm() {
     },
   })
 
-  const { register, handleSubmit, reset } = useForm()
+  const {
+    register,
+    handleSubmit,
+    reset,
+    getValues,
+    formState: { errors },
+  } = useForm()
 
   function onSubmit(data) {
     mutate(data)
   }
 
+  function onError(errors) {
+    console.log(errors)
+  }
+
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <FormRow>
-        <Label htmlFor='name'>Cabin name</Label>
-        <Input type='text' id='name' {...register("name")} />
+    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+      <FormRow label='Cabin name' error={errors?.name?.message}>
+        <Input
+          type='text'
+          id='name'
+          {...register("name", { required: "This field is required" })}
+        />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor='maxCapacity'>Maximum capacity</Label>
-        <Input type='number' id='maxCapacity' {...register("maxCapacity")} />
+      <FormRow label='Maximum Capacity' error={errors?.maxCapacity?.message}>
+        <Input
+          type='number'
+          id='maxCapacity'
+          {...register("maxCapacity", {
+            required: "This field is required",
+            min: {
+              value: 1,
+              message: "Capacity should be at least 1",
+            },
+          })}
+        />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor='regularPrice'>Regular price</Label>
-        <Input type='number' id='regularPrice' {...register("regularPrice")} />
+      <FormRow label='Regular Price' error={errors?.regularPrice?.message}>
+        <Input
+          type='number'
+          id='regularPrice'
+          {...register("regularPrice", { required: "This field is required" })}
+        />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor='discount'>Discount</Label>
+      <FormRow label='Discount' error={errors?.discount?.message}>
         <Input
           type='number'
           id='discount'
           defaultValue={0}
-          {...register("discount")}
+          {...register("discount", {
+            required: "This field is required",
+            validate: (value) =>
+              value <= getValues().regularPrice ||
+              "Discount should be less than regular price",
+          })}
         />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor='description'>Description for website</Label>
+      <FormRow
+        label='Description for website'
+        error={errors?.description?.message}
+      >
         <Textarea
           type='text'
           id='description'
           defaultValue=''
-          {...register("description")}
+          {...register("description", { required: "This field is required" })}
         />
       </FormRow>
 
--- 19-The-wild-oasis/src/ui/FormRow.jsx
+++ 19-The-wild-oasis/src/ui/FormRow.jsx
@@ -1,12 +1,10 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components"
 
 const StyledFormRow = styled.div`
   display: grid;
   align-items: center;
-
-  grid-template-columns: ${(props) =>
-    props.orientation === "vertical" ? "1fr" : "24rem 1fr 1.2fr"};
-  gap: ${(props) => (props.orientation === "vertical" ? "0.8rem" : "2.4rem")};
+  grid-template-columns: 24rem 1fr 1.2fr;
+  gap: 2.4rem;
 
   padding: 1.2rem 0;
 
@@ -19,41 +17,33 @@ const StyledFormRow = styled.div`
   }
 
   &:not(:last-child) {
-    border-bottom: ${(props) =>
-      props.orientation === "vertical"
-        ? "none"
-        : "1px solid var(--color-grey-100)"};
+    border-bottom: 1px solid var(--color-grey-100);
   }
 
-  /* Special treatment if the row contains buttons, and if it's NOT a vertical row */
-  ${(props) =>
-    props.orientation !== "vertical" &&
-    css`
-      &:has(button) {
-        display: flex;
-        justify-content: flex-end;
-        gap: 1.2rem;
-      }
-    `}
-`;
+  &:has(button) {
+    display: flex;
+    justify-content: flex-end;
+    gap: 1.2rem;
+  }
+`
 
-const Label = styled.label`
+const Label = styled.div`
   font-weight: 500;
-`;
+`
 
 const Error = styled.span`
   font-size: 1.4rem;
   color: var(--color-red-700);
-`;
+`
 
-function FormRow({ label, error, children, orientation }) {
+function FormRow({ children, label, error }) {
   return (
-    <StyledFormRow orientation={orientation}>
+    <StyledFormRow>
       {label && <Label htmlFor={children.props.id}>{label}</Label>}
       {children}
       {error && <Error>{error}</Error>}
     </StyledFormRow>
-  );
+  )
 }
 
-export default FormRow;
+export default FormRow
