9247aa9b6a4ad0c235bc15bd6673983f7296427b
Akash-ninja <akash.gitcode@gmail.com>
2024-05-28 08:52:11 +0530

    React query code made into custom hooks

--- 19-The-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 19-The-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -1,10 +1,9 @@
-import styled from "styled-components"
-import { formatCurrency } from "../../utils/helpers"
-import { useMutation, useQueryClient } from "@tanstack/react-query"
-import { deleteCabin } from "../../services/apiCabins"
-import toast from "react-hot-toast"
 import { useState } from "react"
+import styled from "styled-components"
+
 import NewCabinForm from "./NewCabinForm"
+import { formatCurrency } from "../../utils/helpers"
+import { useDeleteCabin } from "./useDeleteCabin"
 
 const TableRow = styled.div`
   display: grid;
@@ -57,19 +56,7 @@ function CabinRow({ cabin }) {
     image,
   } = cabin
 
-  const queryClient = useQueryClient()
-
-  const { isLoading: isDeleting, mutate } = useMutation({
-    mutationFn: deleteCabin,
-    onSuccess: () => {
-      toast.success("Cabin successfully deleted")
-
-      queryClient.invalidateQueries({
-        queryKey: ["cabins"],
-      })
-    },
-    onError: (err) => toast.error(err.message),
-  })
+  const { isDeleting, deleteCabin } = useDeleteCabin()
 
   return (
     <>
@@ -78,10 +65,14 @@ function CabinRow({ cabin }) {
         <Cabin>{name}</Cabin>
         <div>Fits up to {maxCapacity} guests</div>
         <Price>{formatCurrency(regularPrice)}</Price>
-        <Discount>{formatCurrency(discount)}</Discount>
+        {discount ? (
+          <Discount>{formatCurrency(discount)}</Discount>
+        ) : (
+          <span>&mdash;</span>
+        )}
         <div>
           <button onClick={() => setShowForm((show) => !show)}>Edit</button>
-          <button onClick={() => mutate(cabinId)} disabled={isDeleting}>
+          <button onClick={() => deleteCabin(cabinId)} disabled={isDeleting}>
             Delete
           </button>
         </div>
--- 19-The-wild-oasis/src/features/cabins/NewCabinForm.jsx
+++ 19-The-wild-oasis/src/features/cabins/NewCabinForm.jsx
@@ -1,7 +1,4 @@
-import styled from "styled-components"
-import toast from "react-hot-toast"
 import { useForm } from "react-hook-form"
-import { useMutation, useQueryClient } from "@tanstack/react-query"
 
 import Input from "../../ui/Input"
 import Form from "../../ui/Form"
@@ -10,17 +7,16 @@ import FileInput from "../../ui/FileInput"
 import Textarea from "../../ui/Textarea"
 import FormRow from "../../ui/FormRow"
 
-import { createEditCabin } from "../../services/apiCabins"
-
-const Label = styled.div`
-  font-weight: 500;
-`
+import { useCreateCabin } from "./useCreateCabin"
+import { useEditCabin } from "./useEditCabin"
 
 function NewCabinForm({ cabinToEdit = {} }) {
+  const { isCreating, createCabin } = useCreateCabin()
+  const { isEditing, editCabin } = useEditCabin()
+  const isWorking = isCreating || isEditing
+
   const { id: editId, ...editValues } = cabinToEdit
   const isEditSession = Boolean(editId)
-
-  const queryClient = useQueryClient()
 
   const {
     register,
@@ -32,38 +28,12 @@ function NewCabinForm({ cabinToEdit = {} }) {
     defaultValues: isEditSession ? editValues : {},
   })
 
-  const { mutate: createCabin, isLoading: isCreating } = useMutation({
-    mutationFn: createEditCabin,
-    onSuccess: () => {
-      toast.success("New cabin successfully created.")
-      queryClient.invalidateQueries({ queryKey: ["cabins"] })
-      reset()
-    },
-    onError: (err) => {
-      toast.error(err.message)
-    },
-  })
-
-  const { mutate: editCabin, isLoading: isEditing } = useMutation({
-    mutationFn: ({ newCabinData, id }) => createEditCabin(newCabinData, id),
-    onSuccess: () => {
-      toast.success("Cabin successfully edited.")
-      queryClient.invalidateQueries({ queryKey: ["cabins"] })
-      reset()
-    },
-    onError: (err) => {
-      toast.error(err.message)
-    },
-  })
-
-  const isWorking = isCreating || isEditing
-
   function onSubmit(data) {
     const image = typeof data.image === "string" ? data.image : data.image[0]
 
     if (isEditSession)
       editCabin({ newCabinData: { ...data, image }, id: editId })
-    else createCabin({ ...data, image })
+    else createCabin({ ...data, image }, { onSuccess: (data) => reset() })
   }
 
   function onError(errors) {
