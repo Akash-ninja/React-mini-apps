682dc4bb3dcd2d96f8e090b44bfd6897eaddf74d
Akash-ninja <akash.gitcode@gmail.com>
2024-09-01 17:34:54 +0530

    Demo - Higher Order Component Pattern

--- 23-Advanced-React-Patterns/src/App.js
+++ 23-Advanced-React-Patterns/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { faker } from "@faker-js/faker"
 import "./styles.css"
+import withToggles from "./HOC"
 
 const products = Array.from({ length: 20 }, () => {
   return {
@@ -78,12 +79,14 @@ function List({ title, items = [], render }) {
 }
 
 export default function App() {
+  const ProductListWithToggles = withToggles(ProductList)
+
   return (
     <div>
       <h1>Render Props Demo</h1>
 
       {/* Render props passed */}
-      <div className='col-2'>
+      {/*  <div className='col-2'>
         <List
           title='Products'
           items={products}
@@ -103,7 +106,24 @@ export default function App() {
             />
           )}
         />
+      </div> */}
+
+      <div className='col-2'>
+        <ProductList title='Products HOC' items={products} />
+        <ProductListWithToggles title='Products HOC' items={products} />
       </div>
     </div>
+  )
+}
+
+// LATER: Let's say we got this component from a 3rd-party library,
+// and can't change it. But we still want to add the 2 toggle functionalities to it.
+function ProductList({ title, items }) {
+  return (
+    <ul className='list'>
+      {items.map((product) => (
+        <ProductItem key={product.productName} product={product} />
+      ))}
+    </ul>
   )
 }
