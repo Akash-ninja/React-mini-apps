2ac848565e953b097a794385f775806f0efc9e2f
Akash-ninja <akash.gitcode@gmail.com>
2024-09-15 11:19:46 +0530

    Made Table component resusable (using CC pattern)

--- 19-The-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 19-The-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -8,18 +8,7 @@ import { HiSquare2Stack, HiPencil, HiTrash } from "react-icons/hi2"
 import { useCreateCabin } from "./useCreateCabin"
 import Modal from "../../ui/Modal"
 import ConfirmDelete from "../../ui/ConfirmDelete"
-
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`
+import Table from "../../ui/Table"
 
 const Img = styled.img`
   display: block;
@@ -75,7 +64,7 @@ function CabinRow({ cabin }) {
   const { isDeleting, deleteCabin } = useDeleteCabin()
 
   return (
-    <TableRow role='row'>
+    <Table.Row>
       <Img src={image} />
       <Cabin>{name}</Cabin>
       <div>Fits up to {maxCapacity} guests</div>
@@ -114,7 +103,7 @@ function CabinRow({ cabin }) {
           </Modal.Window>
         </Modal>
       </div>
-    </TableRow>
+    </Table.Row>
   )
 }
 
