e330a70fa8b9554ef8a5fb26be18d5735e6fed7c
Akash-ninja <akash.gitcode@gmail.com>
2024-09-29 08:00:05 +0530

    Displaying: Row data according to pagination (API-side pagination)

--- 19-The-wild-oasis/src/services/apiBookings.js
+++ 19-The-wild-oasis/src/services/apiBookings.js
@@ -1,7 +1,8 @@
+import { PAGE_SIZE } from "../utils/constants"
 import { getToday } from "../utils/helpers"
 import supabase from "./supabase"
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
     .from("bookings")
     .select(
@@ -18,6 +19,12 @@ export async function getBookings({ filter, sortBy }) {
     query = query.order(sortBy.field, {
       ascending: sortBy.direction === "asc",
     })
+
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE
+    const to = from + PAGE_SIZE - 1
+    query = query.range(from, to)
+  }
 
   const { data, error, count } = await query
 
