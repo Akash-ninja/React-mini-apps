2405016cf65f1d13d2e6cb1154afdcd3af136dc5
Akash-ninja <akash.gitcode@gmail.com>
2024-10-02 15:35:38 +0530

    BookinsTable: Starter files commit

--- 19-The-wild-oasis/src/features/bookings/BookingDetail.jsx
+++ 19-The-wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -1,22 +1,14 @@
-import { useNavigate } from "react-router-dom"
 import styled from "styled-components"
 
-import Spinner from "ui/Spinner"
 import BookingDataBox from "./BookingDataBox"
-import Row from "ui/Row"
-import Heading from "ui/Heading"
-import Tag from "ui/Tag"
-import ButtonGroup from "ui/ButtonGroup"
-import Button from "ui/Button"
-import Modal from "ui/Modal"
-import ConfirmDelete from "ui/ConfirmDelete"
+import Row from "../../ui/Row"
+import Heading from "../../ui/Heading"
+import Tag from "../../ui/Tag"
+import ButtonGroup from "../../ui/ButtonGroup"
+import Button from "../../ui/Button"
+import ButtonText from "../../ui/ButtonText"
 
-import { useBooking } from "features/bookings/useBooking"
-import { useDeleteBooking } from "./useDeleteBooking"
-import { useMoveBack } from "hooks/useMoveBack"
-import { useCheckout } from "features/check-in-out/useCheckout"
-import ButtonText from "ui/ButtonText"
-import Empty from "ui/Empty"
+import { useMoveBack } from "../../hooks/useMoveBack"
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -25,15 +17,10 @@ const HeadingGroup = styled.div`
 `
 
 function BookingDetail() {
-  const { booking } = useBooking()
-  const { mutate: deleteBooking, isLoading: isDeleting } = useDeleteBooking()
-  const { mutate: checkout, isLoading: isCheckingOut } = useCheckout()
+  const booking = {}
+  const status = "checked-in"
 
   const moveBack = useMoveBack()
-  const navigate = useNavigate()
-
-  // if (isLoading) return <Spinner />;
-  // if (!booking) return <Empty resource='booking' />;
 
   const statusToTagName = {
     unconfirmed: "blue",
@@ -41,14 +28,11 @@ function BookingDetail() {
     "checked-out": "silver",
   }
 
-  const { id: bookingId, status } = booking
-
-  // We return a fragment so that these elements fit into the page's layout
   return (
     <>
       <Row type='horizontal'>
         <HeadingGroup>
-          <Heading type='h1'>Booking #{bookingId}</Heading>
+          <Heading as='h1'>Booking #X</Heading>
           <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
         </HeadingGroup>
         <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
@@ -57,32 +41,6 @@ function BookingDetail() {
       <BookingDataBox booking={booking} />
 
       <ButtonGroup>
-        {status === "unconfirmed" && (
-          <Button onClick={() => navigate(`/checkin/${bookingId}`)}>
-            Check in
-          </Button>
-        )}
-
-        {status === "checked-in" && (
-          <Button onClick={() => checkout(bookingId)} disabled={isCheckingOut}>
-            Check out
-          </Button>
-        )}
-
-        <Modal>
-          <Modal.Toggle opens='delete'>
-            <Button variation='danger'>Delete booking</Button>
-          </Modal.Toggle>
-          <Modal.Window name='delete'>
-            <ConfirmDelete
-              resource='booking'
-              // These options will be passed wherever the function gets called, and they determine what happens next
-              onConfirm={(options) => deleteBooking(bookingId, options)}
-              disabled={isDeleting}
-            />
-          </Modal.Window>
-        </Modal>
-
         <Button variation='secondary' onClick={moveBack}>
           Back
         </Button>
