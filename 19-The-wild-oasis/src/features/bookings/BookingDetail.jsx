f4d492b367ab0e33c065f0ebcb7fe891730845dc
Akash-ninja <akash.gitcode@gmail.com>
2024-10-26 21:21:05 +0530

    Injected New Route: For Check-in page

--- 19-The-wild-oasis/src/features/bookings/BookingDetail.jsx
+++ 19-The-wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -11,6 +11,7 @@ import Spinner from "../../ui/Spinner"
 
 import { useMoveBack } from "../../hooks/useMoveBack"
 import { useBooking } from "./useBooking"
+import { useNavigate } from "react-router-dom"
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -21,6 +22,7 @@ const HeadingGroup = styled.div`
 function BookingDetail() {
   const { booking, isLoading } = useBooking()
   const moveBack = useMoveBack()
+  const navigate = useNavigate()
 
   if (isLoading) return <Spinner />
 
@@ -45,6 +47,12 @@ function BookingDetail() {
       <BookingDataBox booking={booking} />
 
       <ButtonGroup>
+        {status === "unconfirmed" && (
+          <Button onClick={() => navigate(`/checkin/${bookingId}`)}>
+            Check in
+          </Button>
+        )}
+
         <Button variation='secondary' onClick={moveBack}>
           Back
         </Button>
--- 19-The-wild-oasis/src/features/bookings/BookingRow.jsx
+++ 19-The-wild-oasis/src/features/bookings/BookingRow.jsx
@@ -6,7 +6,7 @@ import Table from "../../ui/Table"
 import { formatCurrency, formatDistanceFromNow } from "../../utils/helpers"
 import { format, isToday } from "date-fns"
 import Menus from "../../ui/Menus"
-import { HiEye } from "react-icons/hi2"
+import { HiArrowDownOnSquare, HiEye } from "react-icons/hi2"
 import { useNavigate } from "react-router-dom"
 
 const Cabin = styled.div`
@@ -93,6 +93,15 @@ function BookingRow({
           >
             See Details
           </Menus.Button>
+
+          {status === "unconfirmed" && (
+            <Menus.Button
+              icon={<HiArrowDownOnSquare />}
+              onClick={() => navigate(`/checkin/${bookingId}`)}
+            >
+              Check in
+            </Menus.Button>
+          )}
         </Menus.List>
       </Menus.Menu>
     </Table.Row>
