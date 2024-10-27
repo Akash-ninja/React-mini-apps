cc66dfad34bd70c36f88852054d21631bd98f789
Akash Keshari <akash.gitcode@gmail.com>
2024-10-27 14:22:15 +0530

    App-19 [PR-5]: Implementation of Booking Detail Page and its features (#28)
    * Injected New Route: To go to single Booking detail
    
    * Implemented: Booking detail page, fetch data and display
    
    * Injected New Route: For Check-in page
    
    * Implemented: UI for Check In
    
    * Implemented: Check in functionality
    
    * Implemented: Adding Optional Breakfast in a Booking
    
    * Implemented: Checkout from Booking Row and Detail page
    
    * Fixes: Filter issue with page
    
    * Implemented: Delete a booking (from Booking Row and Details page)

--- 19-The-wild-oasis/src/features/bookings/BookingRow.jsx
+++ 19-The-wild-oasis/src/features/bookings/BookingRow.jsx
@@ -1,10 +1,22 @@
 import styled from "styled-components"
+import { useNavigate } from "react-router-dom"
+import { format, isToday } from "date-fns"
+import {
+  HiArrowDownOnSquare,
+  HiArrowUpOnSquare,
+  HiEye,
+  HiTrash,
+} from "react-icons/hi2"
 
 import Tag from "../../ui/Tag"
 import Table from "../../ui/Table"
+import Menus from "../../ui/Menus"
+import Modal from "../../ui/Modal"
+import ConfirmDelete from "../../ui/ConfirmDelete"
 
 import { formatCurrency, formatDistanceFromNow } from "../../utils/helpers"
-import { format, isToday } from "date-fns"
+import { useCheckout } from "../check-in-out/useCheckout"
+import { useDeleteBooking } from "./useDeleteBooking"
 
 const Cabin = styled.div`
   font-size: 1.6rem;
@@ -47,6 +59,10 @@ function BookingRow({
     cabins: { name: cabinName },
   },
 }) {
+  const navigate = useNavigate()
+  const { checkout, isCheckingOut } = useCheckout()
+  const { deleteBooking, isDeleting } = useDeleteBooking()
+
   const statusToTagName = {
     unconfirmed: "blue",
     "checked-in": "green",
@@ -78,6 +94,51 @@ function BookingRow({
       <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
 
       <Amount>{formatCurrency(totalPrice)}</Amount>
+
+      <Modal>
+        <Menus.Menu>
+          <Menus.Toggle id={bookingId} />
+          <Menus.List id={bookingId}>
+            <Menus.Button
+              icon={<HiEye />}
+              onClick={() => navigate(`/bookings/${bookingId}`)}
+            >
+              See Details
+            </Menus.Button>
+
+            {status === "unconfirmed" && (
+              <Menus.Button
+                icon={<HiArrowDownOnSquare />}
+                onClick={() => navigate(`/checkin/${bookingId}`)}
+              >
+                Check in
+              </Menus.Button>
+            )}
+
+            {status === "checked-in" && (
+              <Menus.Button
+                icon={<HiArrowUpOnSquare />}
+                onClick={() => checkout(bookingId)}
+                disabled={isCheckingOut}
+              >
+                Check Out
+              </Menus.Button>
+            )}
+
+            <Modal.Open opens='delete'>
+              <Menus.Button icon={<HiTrash />}>Delete booking</Menus.Button>
+            </Modal.Open>
+          </Menus.List>
+        </Menus.Menu>
+
+        <Modal.Window name='delete'>
+          <ConfirmDelete
+            resourceName='booking'
+            onConfirm={() => deleteBooking(bookingId)}
+            disabled={isDeleting}
+          />
+        </Modal.Window>
+      </Modal>
     </Table.Row>
   )
 }
--- 19-The-wild-oasis/src/features/check-in-out/CheckinBooking.jsx
+++ 19-The-wild-oasis/src/features/check-in-out/CheckinBooking.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react"
 import styled from "styled-components"
 import BookingDataBox from "../../features/bookings/BookingDataBox"
 
@@ -8,6 +9,12 @@ import Button from "../../ui/Button"
 import ButtonText from "../../ui/ButtonText"
 
 import { useMoveBack } from "../../hooks/useMoveBack"
+import { useBooking } from "../bookings/useBooking"
+import { formatCurrency } from "../../utils/helpers"
+import Spinner from "../../ui/Spinner"
+import Checkbox from "../../ui/Checkbox"
+import { useCheckin } from "./useCheckin"
+import { useSettings } from "../settings/useSettings"
 
 const Box = styled.div`
   /* Box */
@@ -18,9 +25,17 @@ const Box = styled.div`
 `
 
 function CheckinBooking() {
-  const moveBack = useMoveBack()
+  const [confirmPaid, setConfirmPaid] = useState(false)
+  const [addBreakfast, setAddBreakfast] = useState()
+  const { booking, isLoading } = useBooking()
+  const { settings, isLoading: isLoadingSettings } = useSettings()
 
-  const booking = {}
+  useEffect(() => setConfirmPaid(booking?.isPaid ?? false), [booking])
+
+  const moveBack = useMoveBack()
+  const { checkin, isCheckingIn } = useCheckin()
+
+  if (isLoading || isLoadingSettings) return <Spinner />
 
   const {
     id: bookingId,
@@ -31,7 +46,24 @@ function CheckinBooking() {
     numNights,
   } = booking
 
-  function handleCheckin() {}
+  const optionalBreakfastPrice = settings.breakfastPrice * numNights * numGuests
+
+  function handleCheckin() {
+    if (!confirmPaid) return
+
+    if (addBreakfast) {
+      checkin({
+        bookingId,
+        breakfast: {
+          hasBreakfast: true,
+          extrasPrice: optionalBreakfastPrice,
+          totalPrice: totalPrice + optionalBreakfastPrice,
+        },
+      })
+    } else {
+      checkin({ bookingId, breakfast: {} })
+    }
+  }
 
   return (
     <>
@@ -42,8 +74,43 @@ function CheckinBooking() {
 
       <BookingDataBox booking={booking} />
 
+      {!hasBreakfast && (
+        <Box>
+          <Checkbox
+            checked={addBreakfast}
+            onChange={() => {
+              setAddBreakfast((add) => !add)
+              setConfirmPaid(false)
+            }}
+            id='breakfast'
+          >
+            Want to add breakfast for {formatCurrency(optionalBreakfastPrice)}?
+          </Checkbox>
+        </Box>
+      )}
+
+      <Box>
+        <Checkbox
+          checked={confirmPaid}
+          disabled={confirmPaid || isCheckingIn}
+          onChange={() => setConfirmPaid((confirm) => !confirm)}
+          id='confirm'
+        >
+          I confirm that {guests.fullName} has paid the total amount of{" "}
+          {!addBreakfast
+            ? formatCurrency(totalPrice)
+            : `${formatCurrency(
+                totalPrice + optionalBreakfastPrice
+              )} (${formatCurrency(totalPrice)} + ${formatCurrency(
+                optionalBreakfastPrice
+              )})`}
+        </Checkbox>
+      </Box>
+
       <ButtonGroup>
-        <Button onClick={handleCheckin}>Check in booking #{bookingId}</Button>
+        <Button onClick={handleCheckin} disabled={!confirmPaid || isCheckingIn}>
+          Check in booking #{bookingId}
+        </Button>
         <Button variation='secondary' onClick={moveBack}>
           Back
         </Button>
