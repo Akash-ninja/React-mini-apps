db95b2dc400b0ca5489211755971f1bb0b352773
Akash-ninja <akash.gitcode@gmail.com>
2024-11-01 11:54:52 +0530

    Fixes: A bug which was not letting 'isAuthenticated' to true because of wrong method used

--- 19-The-wild-oasis/src/features/authentication/useLogin.js
+++ 19-The-wild-oasis/src/features/authentication/useLogin.js
@@ -10,8 +10,8 @@ export function useLogin() {
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
 
-    onSuccess: (user) => {
-      queryClient.setQueriesData(["user"], user)
+    onSuccess: (data) => {
+      queryClient.setQueryData(["user"], data?.user)
       navigate("/dashboard", { replace: true })
     },
 
