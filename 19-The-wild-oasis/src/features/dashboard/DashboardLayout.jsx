4a7a7e326e4d5c13b44963cd50f5c5fbdbb871e4
Akash-ninja <akash.gitcode@gmail.com>
2024-11-05 22:57:23 +0530

    Computed: Recent Bookings and Stays

--- 19-The-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 19-The-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components"
+import { useRecentBookings } from "./useRecentBookings"
+import { useRecentStays } from "./useRecentStays"
+import Spinner from "../../ui/Spinner"
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -8,6 +11,13 @@ const StyledDashboardLayout = styled.div`
 `
 
 function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } = useRecentBookings()
+  const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays()
+
+  if (isLoading1 || isLoading2) return <Spinner />
+
+  console.log(bookings)
+
   return (
     <StyledDashboardLayout>
       <div>Statistics</div>
