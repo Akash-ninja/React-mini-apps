1bf2ae33dafac0ab5b8df1227ff6c5c2823a44d6
Akash-ninja <akash.gitcode@gmail.com>
2024-11-26 21:20:46 +0530

    Implemented: Duration Chart with Pie charts

--- 19-The-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 19-The-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useCabins } from "../cabins/useCabins"
 import Spinner from "../../ui/Spinner"
 import Stats from "./Stats"
 import SalesChart from "./SalesChart"
+import DurationChart from "./DurationChart"
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -34,7 +35,7 @@ function DashboardLayout() {
         cabinCount={cabins.length}
       />
       <div>Today's activity</div>
-      <div>Chart stay durations</div>
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   )
