d81d1e730e640b93ce24a27074af34d7007f8d3b
Akash-ninja <akash.gitcode@gmail.com>
2025-11-22 20:41:30 +0530

    Optimised Images with Nextjs component

--- 22-The-wild-oasis-website/app/_components/Logo.js
+++ 22-The-wild-oasis-website/app/_components/Logo.js
@@ -1,9 +1,10 @@
+import Image from "next/image"
 import Link from "next/link"
 
 function Logo() {
   return (
     <Link href='/' className='flex items-center gap-4 z-10'>
-      <img src='/logo.png' height='60' width='60' alt='The Wild Oasis logo' />
+      <Image src='/logo.png' height='60' width='60' alt='The Wild Oasis logo' />
       <span className='text-xl font-semibold text-primary-100'>
         The Wild Oasis
       </span>
@@ -12,3 +13,10 @@ function Logo() {
 }
 
 export default Logo
+
+/* 
+Image component does the following:
+1. It will automatically serve correctly sized images in modern formats like WebP (on demand)
+2. Image component prevent layout shifts because it requires width and height to be specified
+3. Lazy loads the images
+ */
