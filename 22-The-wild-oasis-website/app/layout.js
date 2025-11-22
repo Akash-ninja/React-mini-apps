07794ec02884adf4df72242106182db3f163810d
Akash-ninja <akash.gitcode@gmail.com>
2025-11-22 16:09:00 +0530

    Added: Page Metadata and Favicon alongwith my own tailwind colors

--- 22-The-wild-oasis-website/app/layout.js
+++ 22-The-wild-oasis-website/app/layout.js
@@ -1,14 +1,20 @@
 import Logo from "@/app/_components/Logo"
 import Navigation from "@/app/_components/Navigation"
+import "@/app/_styles/globals.css"
 
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body>
+      <body className='bg-primary-950 text-primary-100 min-h-screen'>
         <header>
           <Logo />
           <Navigation />
