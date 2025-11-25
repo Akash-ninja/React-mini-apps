f9f5daee16ab46e01e7e372080dfe98293f5d07d
Akash-ninja <akash.gitcode@gmail.com>
2025-11-26 02:31:02 +0530

    Added: Rendered dynamic route page and metadata

--- 22-The-wild-oasis-website/app/cabins/[cabinId]/page.js
+++ 22-The-wild-oasis-website/app/cabins/[cabinId]/page.js
@@ -1,19 +1,17 @@
+import Image from "next/image"
+import { getCabin } from "@/app/_lib/data-service"
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid"
 
-// PLACEHOLDER DATA
-const cabin = {
-  id: 89,
-  name: "001",
-  maxCapacity: 2,
-  regularPrice: 250,
-  discount: 0,
-  description:
-    "Discover the ultimate luxury getaway for couples in the cozy wooden cabin 001. Nestled in a picturesque forest, this stunning cabin offers a secluded and intimate retreat. Inside, enjoy modern high-quality wood interiors, a comfortable seating area, a fireplace and a fully-equipped kitchen. The plush king-size bed, dressed in fine linens guarantees a peaceful nights sleep. Relax in the spa-like shower and unwind on the private deck with hot tub.",
-  image:
-    "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg",
+export async function generateMetadata({ params }) {
+  const { name } = await getCabin(params.cabinId)
+  return {
+    title: `Cabin ${name}`,
+  }
 }
 
-export default function Page() {
+export default async function Page({ params }) {
+  const cabin = await getCabin(params.cabinId)
+
   const { id, name, maxCapacity, regularPrice, discount, image, description } =
     cabin
 
@@ -21,7 +19,12 @@ export default function Page() {
     <div className='max-w-6xl mx-auto mt-8'>
       <div className='grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24'>
         <div className='relative scale-[1.15] -translate-x-3'>
-          <img src={image} alt={`Cabin ${name}`} />
+          <Image
+            src={image}
+            fill
+            className='object-cover'
+            alt={`Cabin ${name}`}
+          />
         </div>
 
         <div>
