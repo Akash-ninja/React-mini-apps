c09c2f3411e600913f8f2d51e79504e9f4d596c8
Akash-ninja <akash.gitcode@gmail.com>
2025-11-29 21:31:20 +0530

    Added: Text expander (showcases client-server boundary)

--- 22-The-wild-oasis-website/app/cabins/[cabinId]/page.js
+++ 22-The-wild-oasis-website/app/cabins/[cabinId]/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import { getCabin } from "@/app/_lib/data-service"
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid"
+import TextExpander from "@/app/_components/TextExpander"
 
 export async function generateMetadata({ params }) {
   const { name } = await getCabin(params.cabinId)
@@ -32,7 +33,9 @@ export default async function Page({ params }) {
             Cabin {name}
           </h3>
 
-          <p className='text-lg text-primary-300 mb-10'>{description}</p>
+          <p className='text-lg text-primary-300 mb-10'>
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className='flex flex-col gap-4 mb-7'>
             <li className='flex gap-3 items-center'>
