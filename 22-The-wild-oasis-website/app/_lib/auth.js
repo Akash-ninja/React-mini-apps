b95952746ea25af76d68f5da476b36f6ff25eb88
Akash Keshari <akash.gitcode@gmail.com>
2025-12-06 00:42:10 +0530

    App-22: [PR-4]: Authentication (The wild oasis website)  (#35)
    * Added: authentication setup via Next auth
    
    * Added: Google image for logged-in user (via session)
    
    * Added: Route protection with NextAuth middleware
    
    * Added: Custom login page made workable
    
    * Added: Name in account page rendered dynamically
    
    * Added: sign out feature implemented
    
    * Added guest user creation in DB and session management in authentication flow
    
    * Added: user profile updation using Server action

---
+++ 22-The-wild-oasis-website/app/_lib/auth.js
@@ -0,0 +1,46 @@
+import NextAuth from "next-auth"
+import Google from "next-auth/providers/google"
+import { createGuest, getGuest } from "./data-service"
+
+const authConfig = {
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+  callbacks: {
+    authorized({ auth, request }) {
+      return !!auth?.user
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email)
+
+        if (!existingGuest) {
+          await createGuest({ email: user.email, fullName: user.name })
+        }
+
+        return true
+      } catch {
+        return false
+      }
+    },
+    // central place for getting guest id into the session object for use in the app
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email)
+      session.user.guestId = guest.id
+      return session
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+}
+
+export const {
+  auth,
+  signIn,
+  signOut,
+  handlers: { GET, POST },
+} = NextAuth(authConfig)
--- 22-The-wild-oasis-website/app/account/page.js
+++ 22-The-wild-oasis-website/app/account/page.js
@@ -1,11 +1,16 @@
+import { auth } from "@/app/_lib/auth"
+
 export const metadata = {
   title: "Guest area",
 }
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth()
+  const firstName = session?.user?.name?.split(" ").at(0)
+
   return (
     <h2 className='font-semibold text-2xl text-accent-400 mb-7'>
-      Welcome, Akash
+      Welcome, {firstName}
     </h2>
   )
 }
