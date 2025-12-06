82b4c53ba0742b01c3c9dbe349b6b821e3d43461
Akash-ninja <akash.gitcode@gmail.com>
2025-12-06 13:48:25 +0530

    Displayed: Loading indicator for deletion using "useTransition" hook

--- 22-The-wild-oasis-website/app/_components/DeleteReservation.js
+++ 22-The-wild-oasis-website/app/_components/DeleteReservation.js
@@ -2,15 +2,32 @@
 
 import { TrashIcon } from "@heroicons/react/24/solid"
 import { deleteReservation } from "@/app/_lib/actions"
+import { useTransition } from "react"
+import SpinnerMini from "./SpinnerMini"
 
 function DeleteReservation({ bookingId }) {
+  const [isPending, startTransitiion] = useTransition()
+
+  function handleDelete() {
+    if (confirm("Are you sure you want to delete this reservation?"))
+      startTransitiion(() => deleteReservation(bookingId)) // does not block the UI while side-effect is running
+  }
+
   return (
     <button
-      onClick={() => deleteReservation(bookingId)}
+      onClick={handleDelete}
       className='group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900'
     >
-      <TrashIcon className='h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors' />
-      <span className='mt-1'>Delete</span>
+      {!isPending ? (
+        <>
+          <TrashIcon className='h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors' />
+          <span className='mt-1'>Delete</span>{" "}
+        </>
+      ) : (
+        <span className='mx-auto'>
+          <SpinnerMini />
+        </span>
+      )}
     </button>
   )
 }
