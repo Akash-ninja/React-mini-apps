8b831b2ba9d925f526fe39a22edde2e185a75773
Akash-ninja <akash.gitcode@gmail.com>
2025-12-06 15:13:46 +0530

    Added: Edit cabin implemented

--- 22-The-wild-oasis-website/app/_lib/actions.js
+++ 22-The-wild-oasis-website/app/_lib/actions.js
@@ -1,9 +1,52 @@
 "use server"
 
 import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation"
 import { auth, signIn, signOut } from "./auth"
 import { supabase } from "./supabase"
 import { getBookings } from "./data-service"
+
+export async function updateReservation(formData) {
+  const bookingId = Number(formData.get("bookingId"))
+
+  // 1. Authenticate user
+  const session = await auth()
+  if (!session) throw new Error("Unauthorized to perform this action")
+
+  // 2. Authorization
+  const guestBookings = await getBookings(session.user.guestId)
+  const guestBookingIds = guestBookings.map((booking) => booking.id)
+
+  if (!guestBookingIds.includes(bookingId)) {
+    throw new Error("You are not authorized to edit this booking")
+  }
+
+  // 3. Build update data
+  const updateData = {
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+  }
+
+  // 4. Mutation
+  const { error } = await supabase
+    .from("bookings")
+    .update(updateData)
+    .eq("id", bookingId)
+    .select()
+    .single()
+
+  // 5. Error handling
+  if (error) {
+    throw new Error("Booking could not be updated")
+  }
+
+  // 6. Revalidation
+  revalidatePath(`/account/reservations/edit/${bookingId}`)
+  revalidatePath("/account/reservations")
+
+  // 7. Redirection
+  redirect("/account/reservations")
+}
 
 export async function deleteReservation(bookingId) {
   const session = await auth()
